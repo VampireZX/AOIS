11664e8a8d17807a54236dd7884d37da634c72c3
Kirill <kirill.artukhov@bk.ru>
2023-06-23 17:09:23 +0300

    91

--- aois9/aois9.cpp
+++ aois9/aois9.cpp
@@ -4,7 +4,6 @@
 #define ll long long
 using namespace std;
 
-// hash 1
 int h1(string s, int arrSize)
 {
     ll int hash = 0;
@@ -16,52 +15,12 @@ int h1(string s, int arrSize)
     return hash;
 }
 
-// hash 2
-int h2(string s, int arrSize)
-{
-    ll int hash = 1;
-    for (int i = 0; i < s.size(); i++)
-    {
-        hash = hash + pow(19, i) * s[i];
-        hash = hash % arrSize;
-    }
-    return hash % arrSize;
-}
-
-// hash 3
-int h3(string s, int arrSize)
-{
-    ll int hash = 7;
-    for (int i = 0; i < s.size(); i++)
-    {
-        hash = (hash * 31 + s[i]) % arrSize;
-    }
-    return hash % arrSize;
-}
-
-// hash 4
-int h4(string s, int arrSize)
-{
-    ll int hash = 3;
-    int p = 7;
-    for (int i = 0; i < s.size(); i++) {
-        hash += hash * 7 + s[0] * pow(p, i);
-        hash = hash % arrSize;
-    }
-    return hash;
-}
-
-
 // lookup operation
 bool lookup(bool* bitarray, int arrSize, string s)
 {
     int a = h1(s, arrSize);
-    int b = h2(s, arrSize);
-    int c = h3(s, arrSize);
-    int d = h4(s, arrSize);
 
-    if (bitarray[a] && bitarray[b] && bitarray[c]
-        && bitarray[d])
+    if (bitarray[a])
         return true;
     else
         return false;
@@ -76,20 +35,12 @@ void insert(bool* bitarray, int arrSize, string s)
     else
     {
         int a = h1(s, arrSize);
-        int b = h2(s, arrSize);
-        int c = h3(s, arrSize);
-        int d = h4(s, arrSize);
-
         bitarray[a] = true;
-        bitarray[b] = true;
-        bitarray[c] = true;
-        bitarray[d] = true;
-
         cout << s << " inserted" << endl;
     }
 }
 
-// Driver Code
+// Main func
 int main()
 {
     bool bitarray[100] = { false };
